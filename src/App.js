c3182e127e78198c8edc80b141d86c1b63248cf1
Roshan Karki <roshan@satisfilabs.com>
2024-11-09 13:28:45 -0800

    added aws settings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit123456 <code>src/App.js</code> and save to reload123.
+          Edit12345699 <code>src/App.js</code> and save to reload123.
         </p>
         <a
           className="App-link"
